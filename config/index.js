86a27ba6a21d11b241941284138700ea86478604
Vivek Reddy <vivekanthireddy02@gmail.com>
2021-12-04 22:30:27 -0600

    local

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
       host: "localhost",
       port: 3306,
       user: "root",
-      password: "akshay3895",
-      database: "auctionsystem3",
+      password: "root",
+      database: "auctionsystem",
     },
   },
 };
